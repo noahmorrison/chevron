eb1f041ff382a55359d24342cfac583122ac3922
noah morrison <noah@morrison.ph>
2014-11-22 13:49:27 -0500

    Renamed chevron.py to main.py
    and updated the chevron/__init__.py

--- chevron/__init__.py
+++ chevron/__init__.py
@@ -1,3 +1,5 @@
-from .chevron import main, render, UnclosedSection, cli_main
+from .main import main, cli_main
+from .renderer import render
+from .tokenizer import UnclosedSection
 
 __all__ = ['main', 'render', 'UnclosedSection', 'cli_main']
